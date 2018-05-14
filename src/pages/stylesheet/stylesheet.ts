9a8d847d0b997b1cdf4e88e88a9f88221d1836e4
John Coombs <johnc@backbase.com>
2018-05-14 08:40:20 -0400

    created styelsheet page

---
+++ src/pages/stylesheet/stylesheet.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-stylesheet',
+  templateUrl: 'stylesheet.html'
+})
+export class StyleSheetPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
